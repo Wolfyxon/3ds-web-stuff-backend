f6cf91cb535840d859e1e3b92103aca8107fbf76
Wolfyxon <wolfyxon@gmail.com>
2024-07-07 12:52:30 +0200

    Moved banned words to a separate file

---
+++ lib/bannedWords.ts
@@ -0,0 +1,89 @@
+export const bannedWords: string[] = [ // lord, forgive me for writing those
+    // - English -
+    "fuck",
+    "fuk",
+    "fook",
+    "foock",
+    "fack",
+    "fak",
+
+    "bitch",
+    "bich",
+
+    "whore",
+    "whor",
+
+    "ass",
+
+    "vagina",
+
+    "pussy",
+
+    "penis",
+
+    "dick",
+    "dik",
+    "dic",
+    "diq",
+
+    "sex",
+    "seegz",
+    "segz",
+    "seks",
+    "secs",
+
+    "masturb",
+
+    "shit",
+    "schit",
+
+    "nigger",
+    "niger", // I'm sorry, the country of Niger
+    "niga",
+    "nigga",
+    "niqqer",
+    "niqer",
+    "niqqer",
+
+    "kys",
+
+    // - Polish -
+    "kurw",
+    "kurv",
+    "koorw",
+    "koorv",
+
+    // Note: don't block 'jeb' so people can talk about the Minecraft developer 'Jeb'
+    "zjeb",
+    "jeba",
+    "jebi",
+
+    // Note: don't block 'pierd' because it means 'fart' and is not a swear. Only more advanced variations are swears
+    "pierdo",
+    "pierda",
+
+    "chuj",
+    "huj",
+
+    "pizd",
+    "piźd",
+
+    "gówn",
+    "gown",
+    "guwn",
+    "goown",
+    "gówie",
+    "gowie",
+    "guwie",
+    "goowie",
+
+    "rucha",
+    "ruha",
+    "rooha",
+    "roocha",
+    "rócha",
+    "róha",
+    "roha",
+    "rocha"
+
+];
