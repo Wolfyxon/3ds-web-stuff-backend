ba128bf3ffc8e1bb8ab340f1c24fca9b3d2b923f
Wolfyxon <wolfyxon@gmail.com>
2024-07-07 12:45:09 +0200

    Added polish banned words

--- api/filter.ts
+++ api/filter.ts
@@ -3,6 +3,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 // --== Main logic ==-- //
 
 const bannedWords: string[] = [ // lord, forgive me for writing those
+    // - English -
     "fuck",
     "fuk",
     "fook",
@@ -25,7 +26,47 @@ const bannedWords: string[] = [ // lord, forgive me for writing those
     "dic",
     "diq",
 
-    "kys"
+    "kys",
+
+    // - Polish -
+    "kurw",
+    "kurv",
+    "koorw",
+    "koorv",
+
+    // Note: don't block 'jeb' so people can talk about the Minecraft developer 'Jeb'
+    "zjeb",
+    "jeba",
+    "jebi",
+
+    // Note: don't block 'pierd' because it means 'fart' and is not a swear. Only more advanced variations are swears
+    "pierdo",
+    "pierda",
+
+    "chuj",
+    "huj",
+
+    "pizd",
+    "piźd",
+    
+    "gówn",
+    "gown",
+    "guwn",
+    "goown",
+    "gówie",
+    "gowie",
+    "guwie",
+    "goowie",
+
+    "rucha",
+    "ruha",
+    "rooha",
+    "roocha",
+    "rócha",
+    "róha",
+    "roha",
+    "rocha"
+    
 ];
 
 const banChar = "*";
