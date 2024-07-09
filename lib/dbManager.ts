bd5d617206faef6b8c998994c06a0cc9378ed0e7
Wolfyxon <wolfyxon@gmail.com>
2024-07-09 10:13:13 +0200

    Fixed missing sql statement

--- lib/dbManager.ts
+++ lib/dbManager.ts
@@ -5,7 +5,7 @@ export const MAX_USERNAME_LEN = 32;
 
 
 export async function setup() {
-    await `CREATE TABLE IF NOT EXISTS chat (
+    await sql`CREATE TABLE IF NOT EXISTS chat (
         username VARCHAR(${MAX_USERNAME_LEN}),
         message VARCHAR(${MAX_MESSAGE_LEN}),
         timestamp TIMESTAMP,
