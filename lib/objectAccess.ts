04fe39063e6526d4d6f50da4bc01338b3fc993ee
Wolfyxon <wolfyxon@gmail.com>
2024-07-12 13:01:00 +0200

    Added objectAccess

---
+++ lib/objectAccess.ts
@@ -0,0 +1,11 @@
+function pathAtObject(obj: Object, path: string, separator: string = ".") {
+    const split = path.split(separator);
+    let current = obj;
+
+    split.forEach(sub => {
+        current = current[sub];
+        if(current === undefined) return null;
+    });
+
+    return current;
+}
