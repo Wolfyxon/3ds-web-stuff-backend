d9fdf0986051a21dba589e1ea5dbd078a2a7a934
Wolfyxon <wolfyxon@gmail.com>
2024-07-12 14:09:10 +0200

    Added return type to hash()

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import crypto = require("crypto");
 
-export function hash(str: string) {
+export function hash(str: string): string {
     return crypto.createHash("sha1").update(str).digest("hex");
 }
 
