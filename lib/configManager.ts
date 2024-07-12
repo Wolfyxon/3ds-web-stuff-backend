761fb067b3837aba61e333f52636984210e37868
Wolfyxon <wolfyxon@gmail.com>
2024-07-12 13:04:24 +0200

    Merged objectAccess into configManager (I was planning to add more functions but realized they're not needed)

--- lib/configManager.ts
+++ lib/configManager.ts
@@ -9,6 +9,17 @@ export const configTemplate = JSON.parse(fs.readFileSync(configTemplatePath, 'ut
 
 export type Config = keyof typeof configTemplate;
 
+export function pathAtObject(obj: Object, path: string, separator: string = ".") {
+    const split = path.split(separator);
+    let current = obj;
+
+    split.forEach(sub => {
+        current = current[sub];
+        if(current === undefined) return null;
+    });
+
+    return current;
+}
 
 export function isDebug(): boolean {
     return process.env["DEBUG"] === "1";
