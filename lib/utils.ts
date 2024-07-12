1b03eb45ae8fbdd71ee90c50b606f019d4b6b5b7
Wolfyxon <wolfyxon@gmail.com>
2024-07-12 14:08:59 +0200

    Added parseJSONC()

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,3 +3,7 @@ import crypto = require("crypto");
 export function hash(str: string) {
     return crypto.createHash("sha1").update(str).digest("hex");
 }
+
+export function parseJSONC(jsonc: string): Object {
+    return JSON.parse(jsonc.replace(new RegExp("//.*", 'mg'), ""));
+}
