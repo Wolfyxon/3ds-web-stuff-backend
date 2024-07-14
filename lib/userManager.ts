81188897945646e43fd867f01b2f59e241634f70
Wolfyxon <wolfyxon@gmail.com>
2024-07-14 22:31:00 +0200

    Added getUserByIdPublic() and getUserByNamePublic()

--- lib/userManager.ts
+++ lib/userManager.ts
@@ -23,6 +23,16 @@ export async function getUserByName(username: string) {
     return (await sql`SELECT * FROM users WHERE username=${username}`).rows[0];
 }
 
+export async function getUserByIdPublic(id: number) {
+    await dbMgr.setupUsers();
+    return (await sql`SELECT (id, username, displayName, permissionLevel, createdAt) FROM users WHERE id=${id}`).rows[0];
+}
+
+export async function getUserByNamePublic(username: string) {
+    await dbMgr.setupUsers();
+    return (await sql`SELECT (id, username, displayName, permissionLevel, createdAt) FROM users WHERE username=${username}`).rows[0];
+}
+
 export async function setPermissionLevel(userId: number, permissionLevel: PermissionLevel) {
     console.log(`> Setting permission level of ${userId} to ${permissionLevel}`);
     await sql`UPDATE users SET permissionLevel=${permissionLevel} WHERE id=${userId}`;
