0837cf4206dc9b9a48a7e40bc4a10846db4264d4
Wolfyxon <wolfyxon@gmail.com>
2024-07-14 13:08:59 +0200

    Added ID column

--- lib/dbManager.ts
+++ lib/dbManager.ts
@@ -16,6 +16,7 @@ export async function setupChat() {
 
 export async function setupUsers() {
     await db.query(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         username VARCHAR(${configMgr.get("users.lengthLimits.username") as number}) NOT NULL,
         displayName VARCHAR(${configMgr.get("users.lengthLimits.displayName") as number}),
         passwordHash VARCHAR(64) NOT NULL,
