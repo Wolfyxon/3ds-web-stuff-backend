ee6a1e2af63fa393b7c1d52deb72ebf4be7d9351
Wolfyxon <wolfyxon@gmail.com>
2024-07-14 13:36:12 +0200

    Added isHashed()

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,6 +4,11 @@ export function hash(str: string): string {
     return crypto.createHash("sha1").update(str).digest("hex");
 }
 
+export function isHashed(str: string): boolean {
+    const pattern = /^[a-fA-F0-9]{40}$/;
+    return pattern.test(str);
+}
+
 export function parseJSONC(jsonc: string) {
     return JSON.parse(jsonc.replace(new RegExp("//.*", 'mg'), ""));
 }
