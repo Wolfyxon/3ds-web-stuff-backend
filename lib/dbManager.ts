d9d06c6ed87ba051806fa126b2740064a862677c
Wolfyxon <wolfyxon@gmail.com>
2024-07-14 12:45:27 +0200

    Renamed limit consts

--- lib/dbManager.ts
+++ lib/dbManager.ts
@@ -1,15 +1,15 @@
 import { sql, db } from '@vercel/postgres';
 
-export const MAX_MESSAGE_LEN = 512;
-export const MAX_USERNAME_LEN = 32;
+export const MAX_CHAT_MESSAGE_LEN = 512;
+export const MAX_CHAT_USERNAME_LEN = 32;
 
 
 // NOTE: sql`` doesn't support ${} in such queries
 
 export async function setupChat() {
     await db.query(`CREATE TABLE IF NOT EXISTS chat (
-        username VARCHAR(${MAX_USERNAME_LEN}),
-        message VARCHAR(${MAX_MESSAGE_LEN}),
+        username VARCHAR(${MAX_CHAT_USERNAME_LEN}),
+        message VARCHAR(${MAX_CHAT_MESSAGE_LEN}),
         timestamp TIMESTAMP,
         ipHash VARCHAR(64)
     )`);
