39436a5e3773068ff4fc3ac14e7fb7d6c0fe6e82
Wolfyxon <wolfyxon@gmail.com>
2024-07-15 14:21:38 +0200

    Init users/get endpoint

---
+++ api/users/get.ts
@@ -0,0 +1,34 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+import netUtils = require("../../lib/netUtils");
+import userMgr = require("../../lib/userManager");
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    if(await netUtils.badCheck(req, res)) return;
+
+    const params = new URLSearchParams(new URL("https://localhost" + req.url).search);
+
+    const id = params.get("id") || req.headers["id"];
+    const name = params.get("username") || req.headers["username"];
+
+    if(!id && !name) {
+        return res.status(400).send("No 'username' or 'id' specified in the URL params or the request headers");
+    }
+
+    if(id && name) {
+        return res.status(400).send("'username' and 'id' cannot be present together");
+    }
+
+    if(id) {
+        const numId = parseInt(id as string);
+        
+        if(numId === null || numId === undefined) {
+            return res.status(400).send("Invalid user ID");
+        }
+
+        return res.json(userMgr.getUserByIdPublic(numId));
+    } 
+    else if(name) {
+        return res.json(userMgr.getUserByNamePublic(name as string));
+    }
+}
