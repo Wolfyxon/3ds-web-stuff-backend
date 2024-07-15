49bf145f82f2098de04b57854838dc58ced476c4
Wolfyxon <wolfyxon@gmail.com>
2024-07-15 14:25:45 +0200

    Removed parens

--- lib/userManager.ts
+++ lib/userManager.ts
@@ -25,12 +25,12 @@ export async function getUserByName(username: string) {
 
 export async function getUserByIdPublic(id: number) {
     await dbMgr.setupUsers();
-    return (await sql`SELECT (id, username, displayName, permissionLevel, createdAt) FROM users WHERE id=${id}`).rows[0];
+    return (await sql`SELECT id, username, displayName, permissionLevel, createdAt FROM users WHERE id=${id}`).rows[0];
 }
 
 export async function getUserByNamePublic(username: string) {
     await dbMgr.setupUsers();
-    return (await sql`SELECT (id, username, displayName, permissionLevel, createdAt) FROM users WHERE username=${username}`).rows[0];
+    return (await sql`SELECT id, username, displayName, permissionLevel, createdAt FROM users WHERE username=${username}`).rows[0];
 }
 
 export async function setPermissionLevel(userId: number, permissionLevel: PermissionLevel) {
